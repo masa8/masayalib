3c5fb704ff8012591174a7a323b19d12a69f77fe
masaya shimomatsu <masaya.shimomatsu@gmail.com>
2012-05-02 19:16:01 +0900

    first commit

---
+++ masayalib/masayalib/masayalib.cpp
@@ -0,0 +1,6 @@
+// masayalib.cpp : DLL アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include "masayalib.h"
+
---
+++ masayalib/masayalib/masayalib.h
@@ -0,0 +1,6 @@
+#ifdef MASAYALIB_EXPORTS
+#define MASAYALIB_API __declspec(dllexport)
+#else
+#define MASAYALIB_API __declspec(dllimport)
+#endif
+
